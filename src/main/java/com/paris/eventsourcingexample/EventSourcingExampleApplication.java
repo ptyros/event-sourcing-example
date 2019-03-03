a0396f3ec6f4357a994f566e7e227f2df74d6e0c
paris <ptyros85@gmail.com>
2019-03-03 16:39:38 +0200

    first commit

---
+++ src/main/java/com/paris/eventsourcingexample/EventSourcingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.paris.eventsourcingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSourcingExampleApplication.class, args);
+    }
+
+}
